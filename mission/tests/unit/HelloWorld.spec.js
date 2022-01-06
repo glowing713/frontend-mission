4ec2e699968769a4a45afc6361ab78f8c58b24af
glowing713 <glowing713@naver.com>
2022-01-07 01:28:24 +0900

    chore: Change test lang to kor

--- mission/tests/unit/HelloWorld.spec.js
+++ mission/tests/unit/HelloWorld.spec.js
@@ -2,8 +2,8 @@ import { shallowMount } from '@vue/test-utils';
 import HelloWorld from '@/components/HelloWorld.vue';
 
 describe('HelloWorld.vue', () => {
-  it('Should render a input tag and a button', () => {
-    const wrapper = shallowMount(HelloWorld, {});
+  it('1번 요구사항, 문자열 입력란과 버튼이 있어야 한다', () => {
+    const wrapper = shallowMount(HelloWorld);
     const input = wrapper.find('input');
     const button = wrapper.find('button');
     expect(input.exists()).toBe(true);
