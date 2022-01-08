9befd1f6113edc000bc8b41a4fb73690a499a05c
glowing713 <glowing713@naver.com>
2022-01-08 21:12:48 +0900

    refactor: Refactored test code 중복된 선언과 불필요한 체크 구문들 제거

--- mission/tests/unit/HelloWorld.spec.js
+++ mission/tests/unit/HelloWorld.spec.js
@@ -2,24 +2,16 @@ import { shallowMount } from '@vue/test-utils';
 import HelloWorld from '@/components/HelloWorld.vue';
 
 describe('HelloWorld.vue', () => {
+  const wrapper = shallowMount(HelloWorld);
+  const textInput = wrapper.findComponent({ name: 'base-input' });
+  const button = wrapper.find('button');
+  const textPrint = wrapper.find('p[class="str_print"]');
   it('1번 요구사항, 문자열 입력란과 버튼이 있어야 한다', () => {
-    const wrapper = shallowMount(HelloWorld);
-    const input = wrapper.findComponent({ name: 'base-input' });
-    const button = wrapper.find('button');
-    expect(input.exists()).toBe(true);
+    expect(textInput.exists()).toBe(true);
     expect(button.exists()).toBe(true);
   });
 
   it('2번 요구사항, input에서 입력한 값은 실시간으로 바로 밑에 표시가 된다', async () => {
-    const wrapper = shallowMount(HelloWorld);
-    const textInput = wrapper.findComponent({ name: 'base-input' });
-
-    expect(textInput.exists()).toBe(true);
-
-    const textPrint = wrapper.find('p[class="str_print"]');
-
-    expect(textPrint.exists()).toBe(true);
-
     await textInput.setValue('hello');
     expect(textPrint.text()).toBe('hello');
 
