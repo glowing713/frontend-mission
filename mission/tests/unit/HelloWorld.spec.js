daf5a7ef49052cf863fad6410441cb9d338e181a
glowing713 <glowing713@naver.com>
2022-01-08 21:01:09 +0900

    fix: Fixed test code to pass all cases input 엘리먼트를 컴포넌트화 한 코드에 맞게 테스트코드 수정 input 엘리먼트를 찾던 방식 -> base-input 컴포넌트를 찾는 방식

--- mission/tests/unit/HelloWorld.spec.js
+++ mission/tests/unit/HelloWorld.spec.js
@@ -4,7 +4,7 @@ import HelloWorld from '@/components/HelloWorld.vue';
 describe('HelloWorld.vue', () => {
   it('1번 요구사항, 문자열 입력란과 버튼이 있어야 한다', () => {
     const wrapper = shallowMount(HelloWorld);
-    const input = wrapper.find('input');
+    const input = wrapper.findComponent({ name: 'base-input' });
     const button = wrapper.find('button');
     expect(input.exists()).toBe(true);
     expect(button.exists()).toBe(true);
@@ -12,7 +12,7 @@ describe('HelloWorld.vue', () => {
 
   it('2번 요구사항, input에서 입력한 값은 실시간으로 바로 밑에 표시가 된다', async () => {
     const wrapper = shallowMount(HelloWorld);
-    const textInput = wrapper.find('input[class="str_input"]');
+    const textInput = wrapper.findComponent({ name: 'base-input' });
 
     expect(textInput.exists()).toBe(true);
 
