c9393cd50d731343d4a0b32989af46d88e7d2e30
glowing713 <glowing713@naver.com>
2022-01-09 03:21:15 +0900

    test: Add alert option case to base-button test code

--- mission/tests/unit/BaseButton.spec.js
+++ mission/tests/unit/BaseButton.spec.js
@@ -2,13 +2,21 @@ import { mount } from '@vue/test-utils';
 import BaseButton from '@/components/BaseButton.vue';
 
 describe('BaseButton.vue', () => {
+  const wrapper = mount(BaseButton);
   it('emit check: func prop이 rotate일 때는 rotate trigger', async () => {
-    const wrapper = mount(BaseButton, {
-      props: {
-        func: 'rotate',
-      },
-    });
+    // const wrapper = mount(BaseButton, {
+    //   props: {
+    //     func: 'rotate',
+    //   },
+    // });
+    await wrapper.setProps({ func: 'rotate' });
     await wrapper.trigger('click');
     expect(wrapper.emitted()).toHaveProperty('rotate');
+  });
+
+  it('emit check: func prop이 alert일 때는 alert trigger하고, 누른 횟수 전달', async () => {
+    await wrapper.setProps({ func: 'alert' });
+    await wrapper.trigger('click');
+    expect(wrapper.emitted()).toHaveProperty('alert');
   });
 });
