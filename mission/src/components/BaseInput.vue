a02d1b85eb3b1551654c7696491323d2e0d37757
glowing713 <glowing713@naver.com>
2022-01-09 01:49:48 +0900

    chore: Add modelValue prop

--- mission/src/components/BaseInput.vue
+++ mission/src/components/BaseInput.vue
@@ -1,10 +1,13 @@
 <template>
-    <input type="text" class="base-input" @input="updateText">
+    <input type="text" class="base-input" :value="modelValue" @input="updateText">
 </template>
 
 <script>
 export default {
   name: 'BaseInput',
+  props: {
+    modelValue: String,
+  },
   methods: {
     updateText(e) {
       this.$emit('update:modelValue', e.target.value);
