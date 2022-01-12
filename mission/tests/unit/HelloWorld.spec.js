783fa9aee4cd9fdaecd377774499dc168d662463
glowing713 <glowing713@naver.com>
2022-01-12 19:27:53 +0900

    test: Change component find method get 메소드를 사용하여 엘리먼트가 존재하지 않으면 에러를 띄우도록 설정 testing attribute를 사용하여 엘리먼트를 직접 찾도록 설정 (+ vscode가 vue-cli 프로젝트의 src경로 alias를 잘 인식하도록 jsconfig 수정)

--- mission/tests/unit/HelloWorld.spec.js
+++ mission/tests/unit/HelloWorld.spec.js
@@ -2,44 +2,51 @@ import { mount } from '@vue/test-utils';
 import HelloWorld from '@/components/HelloWorld.vue';
 
 describe('HelloWorld.vue', () => {
-  const wrapper = mount(HelloWorld);
-  const baseInput = wrapper.findComponent({ name: 'base-input' });
-  const baseButton = wrapper.findComponent({ name: 'base-button' });
-  const textPrint = wrapper.find('p[class="str-print"]');
+  let wrapper;
+  let input;
+  let button;
+  let p;
+
+  beforeEach(() => {
+    wrapper = mount(HelloWorld);
+    input = wrapper.get('[data-test="text-input"]');
+    button = wrapper.get('[data-test="rotate-btn"]');
+    p = wrapper.get('[data-test="rotated-txt"]');
+  });
 
   it('1번 요구사항, 문자열 입력란과 버튼이 있어야 한다', () => {
-    expect(baseInput.exists()).toBe(true);
-    expect(baseButton.exists()).toBe(true);
+    expect(input.exists()).toBe(true);
+    expect(button.exists()).toBe(true);
   });
 
   it('2번 요구사항, input에서 입력한 값은 실시간으로 바로 밑에 표시가 된다', async () => {
-    await baseInput.setValue('hello');
-    expect(textPrint.text()).toBe('hello');
+    await input.setValue('hello');
+    expect(p.text()).toBe('hello');
 
-    await baseInput.setValue('hellooo');
-    expect(textPrint.text()).toBe('hellooo');
+    await input.setValue('hellooo');
+    expect(p.text()).toBe('hellooo');
 
-    await baseInput.setValue('123123112');
-    expect(textPrint.text()).toBe('123123112');
+    await input.setValue('123123112');
+    expect(p.text()).toBe('123123112');
 
-    await baseInput.setValue('이건 한글 버전의 테스트 케이스');
-    expect(textPrint.text()).toBe('이건 한글 버전의 테스트 케이스');
+    await input.setValue('이건 한글 버전의 테스트 케이스');
+    expect(p.text()).toBe('이건 한글 버전의 테스트 케이스');
   });
 
   it('3번 요구사항, 버튼을 누르면 입력/출력 문자열이 동시에 왼쪽으로 한칸씩 이동한다', async () => {
-    await baseInput.setValue('banana');
-    await baseButton.trigger('click');
-    expect(baseInput.element.value).toBe('ananab');
-    expect(textPrint.text()).toBe('ananab');
+    await input.setValue('banana');
+    await button.trigger('click');
+    expect(input.element.value).toBe('ananab');
+    expect(p.text()).toBe('ananab');
 
-    await baseInput.setValue('12345');
-    await baseButton.trigger('click');
-    expect(baseInput.element.value).toBe('23451');
-    expect(textPrint.text()).toBe('23451');
+    await input.setValue('12345');
+    await button.trigger('click');
+    expect(input.element.value).toBe('23451');
+    expect(p.text()).toBe('23451');
 
-    await baseInput.setValue('안녕하세요');
-    await baseButton.trigger('click');
-    expect(baseInput.element.value).toBe('녕하세요안');
-    expect(textPrint.text()).toBe('녕하세요안');
+    await input.setValue('안녕하세요');
+    await button.trigger('click');
+    expect(input.element.value).toBe('녕하세요안');
+    expect(p.text()).toBe('녕하세요안');
   });
 });
