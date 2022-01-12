bcf1ae166ea084b29491f27ab1b1a1d7772bb80d
glowing713 <glowing713@naver.com>
2022-01-12 19:47:05 +0900

    test: Add beforeEach and find DOM elmt by data-test att wrapper 객체와 element를 beforeEach 내부에 선언 엘리먼트 찾아오는 방식을 data-test를 사용하도록 변경

--- mission/tests/unit/BaseInput.spec.js
+++ mission/tests/unit/BaseInput.spec.js
@@ -2,29 +2,34 @@ import { shallowMount } from '@vue/test-utils';
 import BaseInput from '@/components/BaseInput.vue';
 
 describe('BaseInput.vue', () => {
-  const wrapper = shallowMount(BaseInput);
-  const inpElement = wrapper.find('input[type=text]');
+  let wrapper;
+  let input;
+  beforeEach(() => {
+    wrapper = shallowMount(BaseInput);
+    input = wrapper.get('[data-test="text-input"]');
+  });
 
   it('이 컴포넌트는 update:modelValue emit 이벤트를 포함해야 한다', async () => {
-    inpElement.trigger('input');
+    input.trigger('input');
     expect(wrapper.emitted()).toHaveProperty('update:modelValue');
   });
 
   it('emit 이벤트의 콜백함수 인자로 전달되는 값이 입력과 동일해야 한다', async () => {
-    inpElement.element.value = 'hello!';
-    inpElement.trigger('input');
-    expect(wrapper.emitted('update:modelValue')[1][0]).toEqual('hello!');
+    input.element.value = 'hello!';
+    input.trigger('input');
+    console.log(wrapper.emitted());
+    expect(wrapper.emitted('update:modelValue')[0][0]).toEqual('hello!');
 
-    inpElement.element.value = 'hello world!야호!';
-    inpElement.trigger('input');
-    expect(wrapper.emitted('update:modelValue')[2][0]).toEqual('hello world!야호!');
+    input.element.value = 'hello world!야호!';
+    input.trigger('input');
+    expect(wrapper.emitted('update:modelValue')[1][0]).toEqual('hello world!야호!');
 
-    inpElement.element.value = 'hello world!야호!12345';
-    inpElement.trigger('input');
-    expect(wrapper.emitted('update:modelValue')[3][0]).toEqual('hello world!야호!12345');
+    input.element.value = 'hello world!야호!12345';
+    input.trigger('input');
+    expect(wrapper.emitted('update:modelValue')[2][0]).toEqual('hello world!야호!12345');
 
-    inpElement.element.value = 'hola!';
-    inpElement.trigger('input');
-    expect(wrapper.emitted('update:modelValue')[4][0]).toEqual('hola!');
+    input.element.value = 'hola!';
+    input.trigger('input');
+    expect(wrapper.emitted('update:modelValue')[3][0]).toEqual('hola!');
   });
 });
