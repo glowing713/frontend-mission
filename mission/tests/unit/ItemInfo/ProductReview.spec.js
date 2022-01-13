d6fc25635c83d56e6731323d5ba5078c63ac8458
glowing713 <glowing713@naver.com>
2022-01-14 00:37:40 +0900

    feat: Add product review component(initialize)

---
+++ mission/tests/unit/ItemInfo/ProductReview.spec.js
@@ -0,0 +1,10 @@
+import { mount } from '@vue/test-utils';
+import ProductReview from '@/components/ProductReview.vue';
+
+describe('ProductReview.vue', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = mount(ProductReview);
+  });
+});
