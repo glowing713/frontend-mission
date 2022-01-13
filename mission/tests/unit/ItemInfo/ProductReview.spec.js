a96a556570934633cf09ab5550e411b47cde2e6f
glowing713 <glowing713@naver.com>
2022-01-14 00:53:31 +0900

    fix: Fix test code typo

--- mission/tests/unit/ItemInfo/ProductReview.spec.js
+++ mission/tests/unit/ItemInfo/ProductReview.spec.js
@@ -1,15 +1,15 @@
-import { mount } from '@vue/test-utils';
+import { shallowMount } from '@vue/test-utils';
 import ProductReview from '@/components/ProductReview.vue';
 
 describe('ProductReview.vue', () => {
   let wrapper;
 
   beforeEach(() => {
-    wrapper = mount(ProductReview);
+    wrapper = shallowMount(ProductReview);
   });
 
   it('renders review contents(texts)', () => {
-    const reviewContent = wrapper.find('[data-test="review-contents]');
+    const reviewContent = wrapper.find('[data-test="review-contents"]');
     expect(reviewContent.exists()).toBe(true);
   });
 });
