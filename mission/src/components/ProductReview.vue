d3e069b3334f06a93fece505f5d5d8289b3d14b2
glowing713 <glowing713@naver.com>
2022-01-14 00:54:08 +0900

    feat: Add review contents div(author, info, text)

---
+++ mission/src/components/ProductReview.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="product-review">
+      <div class="review-contents" data-test="review-contents"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ProductReview',
+  props: {
+    nickname: String,
+    info: Array,
+    review: String,
+  },
+};
+</script>
