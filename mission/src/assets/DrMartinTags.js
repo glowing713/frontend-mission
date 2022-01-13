6c8e6d261e22ae912d24a8cd5d12565bf8948b6d
glowing713 <glowing713@naver.com>
2022-01-13 18:52:49 +0900

    chore: Add saler profile mock tags

---
+++ mission/src/assets/DrMartinTags.js
@@ -0,0 +1,27 @@
+const DrMartinTags = [{
+  id: 1,
+  tag: '#단테',
+}, {
+  id: 2,
+  tag: '#닥터마틴단테',
+}, {
+  id: 3,
+  tag: '#닥터마틴스니커즈',
+}, {
+  id: 4,
+  tag: '#캐주얼',
+}, {
+  id: 5,
+  tag: '#클래식',
+}, {
+  id: 6,
+  tag: '#단화',
+}, {
+  id: 7,
+  tag: '#가죽',
+}, {
+  id: 8,
+  tag: '#레더',
+}];
+
+export default DrMartinTags;
