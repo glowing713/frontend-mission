71ce306748292368373638457f95ecd77894718b
glowing713 <glowing713@naver.com>
2022-01-13 19:12:53 +0900

    feat: Add saler name and saler tags

--- mission/src/views/ItemInfo.vue
+++ mission/src/views/ItemInfo.vue
@@ -1,19 +1,21 @@
 <template>
 <div id='item-info-page'>
   <img src="@/assets/logo.png" alt="product-img" data-test="product-img">
-  <saler-info/>
+  <saler-info :salerName="this.salerName" :salerTags="this.salerTags"/>
 </div>
 </template>
 
 <script>
 import SalerInfo from '@/components/SalerInfo.vue';
+import DrMartinTags from '@/assets/DrMartinTags';
 
 export default {
   name: 'ItemInfoPage',
   components: { SalerInfo },
   data() {
     return {
-
+      salerName: 'Sean',
+      salerTags: DrMartinTags,
     };
   },
   methods: {
