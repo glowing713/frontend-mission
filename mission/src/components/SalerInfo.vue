03db811acc743a0108be7aab2d1e17ec00728645
glowing713 <glowing713@naver.com>
2022-01-13 18:30:29 +0900

    feat: Add saler profile img

--- mission/src/components/SalerInfo.vue
+++ mission/src/components/SalerInfo.vue
@@ -1,5 +1,7 @@
 <template>
-    <div class="saler-info" data-test="saler-info"></div>
+    <div class="saler-info" data-test="saler-info">
+        <img src="@/assets/logo.svg" alt="saler-img" data-test="saler-img">
+    </div>
 </template>
 
 <script>
