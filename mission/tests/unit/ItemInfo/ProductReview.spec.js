b45deac69bbacb107bf2f09482abc956ce18ae3e
glowing713 <glowing713@naver.com>
2022-01-14 18:49:14 +0900

    test: Check review img exists

--- mission/tests/unit/ItemInfo/ProductReview.spec.js
+++ mission/tests/unit/ItemInfo/ProductReview.spec.js
@@ -27,4 +27,9 @@ describe('ProductReview.vue', () => {
     const reviewContent = wrapper.find('[data-test="review-content"]');
     expect(reviewContent.exists()).toBe(true);
   });
+
+  it('renders review image', () => {
+    const reviewImage = wrapper.find('[data-test="review-img"]');
+    expect(reviewImage.exists()).toBe(true);
+  });
 });
