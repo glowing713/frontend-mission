6de3733ce995d17457455e06f92d096a47726cee
glowing713 <glowing713@naver.com>
2022-01-14 23:43:28 +0900

    refactor: Refactor ItemInfo component 상품 리뷰를 컴포넌트화, 판매자 정보와 리뷰를 객체화시킴

--- mission/src/views/ItemInfo.vue
+++ mission/src/views/ItemInfo.vue
@@ -1,36 +1,43 @@
 <template>
   <div id="item-info-page">
     <img src="@/assets/logo.png" alt="product-img" data-test="product-img" />
-    <seller-info :sellerName="this.sellerName" :sellerTags="this.sellerTags" />
+    <seller-info v-bind="this.seller" />
     <div class="product-name" data-test="product-name"></div>
     <div class="product-price" data-test="product-price"></div>
     <div class="product-details" data-test="product-details"></div>
-    <div class="product-reviews" data-test="product-reviews"></div>
+    <div class="product-reviews" data-test="product-reviews">
+      <product-review
+        v-for="(review, idx) in reviews"
+        :key="idx"
+        :nickname="review.nickname"
+        :info="review.info"
+        :imageSrc="review.imageSrc"
+        :review="review.text"
+      />
+    </div>
   </div>
 </template>
 
 <script>
 import SellerInfo from '@/components/SellerInfo.vue';
-import DrMartinTags from '@/assets/DrMartinTags';
+import ProductReview from '@/components/ProductReview.vue';
+
+import DrMartinSeller from '@/assets/DrMartinSeller';
+import DrMartinReviews from '@/assets/DrMartinReviews';
 
 export default {
   name: 'ItemInfoPage',
-  components: { SellerInfo },
+  components: { SellerInfo, ProductReview },
   data() {
     return {
-      sellerName: 'Sean',
-      sellerTags: DrMartinTags,
+      seller: DrMartinSeller,
+      reviews: DrMartinReviews,
     };
   },
-  methods: {
-
-  },
-  computed: {
-
-  },
+  methods: {},
+  computed: {},
 };
 </script>
 
 <style scoped>
-
 </style>
