458c18473e510ac7d747c2b1325095d5889979ec
glowing713 <glowing713@naver.com>
2022-01-14 23:36:42 +0900

    refactor: Change seller info to object

---
+++ mission/src/assets/DrMartinSeller.js
@@ -0,0 +1,7 @@
+import DrMartinTags from './DrMartinTags';
+
+export default {
+  logoSrc: 'https://sunhwan-ably-externship-s3-bucket.s3.ap-northeast-2.amazonaws.com/logo.svg',
+  sellerName: '닥터마틴',
+  sellerTags: DrMartinTags,
+};
