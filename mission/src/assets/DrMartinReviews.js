b1de6a2d62804296fa58422a13784f15ee67397a
glowing713 <glowing713@naver.com>
2022-01-14 23:29:19 +0900

    refactor: Make reviews into object array

---
+++ mission/src/assets/DrMartinReviews.js
@@ -0,0 +1,21 @@
+const reviewObjs = [
+  {
+    nickname: 'externship',
+    info: {
+      height: 175,
+      weight: 70,
+    },
+    imageSrc: 'https://sunhwan-ably-externship-s3-bucket.s3.ap-northeast-2.amazonaws.com/review01.jpeg',
+    text: '너무 이뻐요!!',
+  },
+  {
+    nickname: 'vueisfun',
+    info: {
+      height: 180,
+      weight: 78,
+    },
+    text: '반 사이즈 업으로 사세요~!',
+  },
+];
+
+export default reviewObjs;
