eca43af450b0816d7b1ba8056f36b19d528164af
glowing713 <glowing713@naver.com>
2022-01-15 16:32:03 +0900

    feat: Add like icon in like button

--- mission/src/components/LikeBuyButtons.vue
+++ mission/src/components/LikeBuyButtons.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="sticky-under-buttons">
-    <button data-test="buy-button"></button>
-    <button data-test="like-button">{{ price }}원 구매</button>
+    <button data-test="like-button" @click="onClick" v-html="icon"></button>
+    <button data-test="buy-button">{{ price }}원 구매</button>
   </div>
 </template>
 
@@ -10,6 +10,24 @@ export default {
   name: 'LikeBuyButtons',
   props: {
     price: Number,
+  },
+  data() {
+    return {
+      iconState: true,
+    };
+  },
+  methods: {
+    onClick() {
+      this.iconState = !this.iconState;
+    },
+  },
+  computed: {
+    icon() {
+      if (this.iconState) {
+        return '<i class="far fa-heart" />';
+      }
+      return '<i class="fas fa-heart" />';
+    },
   },
 };
 </script>
