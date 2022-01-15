5aac435a218b858f2f935e3a5bfe08ff051046b1
glowing713 <glowing713@naver.com>
2022-01-15 15:31:24 +0900

    feat: Add like, buy buttons component

---
+++ mission/src/components/LikeBuyButtons.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="sticky-under-buttons">
+    <button data-test="buy-button"></button>
+    <button data-test="like-button">{{ price }}원 구매</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LikeBuyButtons',
+  props: {
+    price: Number,
+  },
+};
+</script>
