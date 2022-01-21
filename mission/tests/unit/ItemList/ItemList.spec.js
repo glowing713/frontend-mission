7e0f5fc9f8137b27f220c4589da8c2789fa8890a
glowing713 <glowing713@naver.com>
2022-01-21 15:07:15 +0900

    test: Check header container rendering

--- mission/tests/unit/ItemList/ItemList.spec.js
+++ mission/tests/unit/ItemList/ItemList.spec.js
@@ -2,9 +2,19 @@ import { mount } from '@vue/test-utils';
 import ItemInfoPage from '@/views/ItemList.vue';
 
 describe('ItemListPage', () => {
-  it('redners ItemListPage', () => {
-    const wrapper = mount(ItemInfoPage);
+  let wrapper;
 
+  beforeEach(() => {
+    wrapper = mount(ItemInfoPage);
+  });
+
+  it('renders ItemListPage', () => {
     expect(wrapper.find('#item-list-page').exists()).toBe(true);
+  });
+
+  it('renders header', () => {
+    const header = wrapper.find('[data-test="header-container"]');
+
+    expect(header.exists()).toBe(true);
   });
 });
