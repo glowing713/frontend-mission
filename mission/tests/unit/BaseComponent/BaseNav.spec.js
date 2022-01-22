68c1da56ee6add9f33dd8ee9b1990dc428db49fb
glowing713 <glowing713@naver.com>
2022-01-22 17:31:30 +0900

    test: Check 5 buttons rendered in nav component

--- mission/tests/unit/BaseComponent/BaseNav.spec.js
+++ mission/tests/unit/BaseComponent/BaseNav.spec.js
@@ -10,11 +10,27 @@ describe('BaseNav', () => {
 
   it('renders nav container', () => {
     const navContainer = wrapper.find('[data-test="nav-container"]');
+
     expect(navContainer.exists()).toBe(true);
   });
 
   it('renders unordered-list element for buttons', () => {
     const ul = wrapper.find('ul');
+
     expect(ul.exists()).toBe(true);
+  });
+
+  it('renders 5 list of buttons', () => {
+    const ul = wrapper.find('ul');
+    const lists = ul.findAll('li');
+
+    expect(lists).toHaveLength(5);
+  });
+
+  it('renders fa icon in each buttons', () => {
+    const ul = wrapper.find('ul');
+    const lists = ul.findAll('li');
+
+    lists.forEach((li) => expect(li.find('i').exists()).toBe(true));
   });
 });
