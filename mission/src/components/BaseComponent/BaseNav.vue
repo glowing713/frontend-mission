1cbedb7108cee424635b004aa264aaaa9cb8abcf
glowing713 <glowing713@naver.com>
2022-01-22 16:57:33 +0900

    feat: Add ul tag for buttons

--- mission/src/components/BaseComponent/BaseNav.vue
+++ mission/src/components/BaseComponent/BaseNav.vue
@@ -1,10 +1,38 @@
 <template>
-  <nav data-test="nav-container"></nav>
+  <nav data-test="nav-container">
+    <ul></ul>
+  </nav>
 </template>
 
 <script>
 export default {
   name: 'BaseNav',
+  data() {
+    return {
+      navButtons: [
+        {
+          id: 'home',
+          html: '<i class="fas fa-home" />',
+        },
+        {
+          id: 'search',
+          html: '<i class="fas fa-search" />',
+        },
+        {
+          id: 'bucket',
+          html: '<i class="fas fa-shopping-cart" />',
+        },
+        {
+          id: 'likes',
+          html: '<i class="fas fa-heart" />',
+        },
+        {
+          id: 'mypage',
+          html: '<i class="fas fa-user" />',
+        },
+      ],
+    };
+  },
 };
 </script>
 
