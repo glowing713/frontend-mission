9a6a7d066719602489645c9a63d5207d59a39620
glowing713 <glowing713@naver.com>
2022-01-22 19:59:22 +0900

    feat: Add price span and prop property

--- mission/src/components/ItemList/Item.vue
+++ mission/src/components/ItemList/Item.vue
@@ -1,13 +1,27 @@
 <template>
   <div class="item-list-item">
-    <div data-test="thumbnail" class="thumbnail-container"></div>
-    <div data-test="content" class="content-container"></div>
+    <div data-test="thumbnail" class="thumbnail-container">
+      <img :src="thumbnail" alt="404 not found">
+    </div>
+    <div data-test="content" class="content-container">
+      <div data-test="price-info" class="price-info">
+        <span data-test="price" class="price"></span>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'ItemListItem',
+  props: {
+    thumbnail: String,
+    price: Number,
+    discountRate: Number,
+    marketName: String,
+    productName: String,
+    sold: Number,
+  },
 };
 </script>
 
