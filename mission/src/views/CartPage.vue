a940bcc56410b34b3e5a9117b113fa6639c9a1d6
glowing713 <glowing713@naver.com>
2022-02-12 03:21:31 +0900

    feat: Add event to go back button

--- mission/src/views/CartPage.vue
+++ mission/src/views/CartPage.vue
@@ -1,12 +1,21 @@
 <template>
   <div class="cart-page" data-test="cart-page">
-      <header data-test="header-container"></header>
+    <header data-test="header-container">
+      <p data-test="go-back-button" @click="goBack">
+        <i :class="['fas', 'fa-chevron-left']"></i>
+      </p>
+    </header>
   </div>
 </template>
 
 <script>
 export default {
   name: 'CartPage',
+  methods: {
+    goBack() {
+      this.$router.back();
+    },
+  },
 };
 </script>
 
