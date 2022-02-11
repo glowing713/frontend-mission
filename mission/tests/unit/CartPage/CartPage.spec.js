880246e8cd223c70645748dc549b51874e888217
glowing713 <glowing713@naver.com>
2022-02-12 03:19:42 +0900

    test: Test redirection to previous page

--- mission/tests/unit/CartPage/CartPage.spec.js
+++ mission/tests/unit/CartPage/CartPage.spec.js
@@ -1,6 +1,13 @@
 import { mount } from '@vue/test-utils';
 import CartPage from '@/views/CartPage.vue';
 
+const mockGetItems = jest.fn();
+const mockGetItemInfo = jest.fn();
+jest.mock('@/repositories/ApiRepository', () => jest.fn().mockImplementation(() => ({
+  getItems: mockGetItems,
+  getItemInfo: mockGetItemInfo,
+})));
+
 describe('CartPage', () => {
   let wrapper;
 
@@ -18,5 +25,24 @@ describe('CartPage', () => {
     const backButton = wrapper.find('[data-test="go-back-button"]');
 
     expect(backButton.exists()).toBe(true);
+  });
+});
+
+describe('Routing test', () => {
+  it('redirects to previous page when back button is clicked', async () => {
+    const mockBack = jest.fn();
+    const wrapper = mount(CartPage, {
+      global: {
+        mocks: {
+          $router: {
+            back: mockBack,
+          },
+        },
+      },
+    });
+
+    await wrapper.find('[data-test="go-back-button"]').trigger('click');
+
+    expect(mockBack).toHaveBeenCalled();
   });
 });
