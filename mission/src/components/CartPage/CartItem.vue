e6278c6ee04a75a722d9e4f9ab481fc521735fcb
glowing713 <glowing713@naver.com>
2022-02-12 22:24:55 +0900

    feat: Add CartItem component with prop data

---
+++ mission/src/components/CartPage/CartItem.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="cart-item-container" data-test="cart-item"></div>
+</template>
+
+<script>
+import cartItemModel from '@/model/CartItem';
+
+export default {
+  name: 'CartItem',
+  props: {
+    item: cartItemModel,
+  },
+};
+</script>
+
+<style>
+</style>
