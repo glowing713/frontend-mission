4567403e18fc3db5c1e1b1b46032f712920a096e
glowing713 <glowing713@naver.com>
2022-02-12 15:51:16 +0900

    style: Add style to order button

--- mission/src/components/CartPage/ContentFooter.vue
+++ mission/src/components/CartPage/ContentFooter.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="sticky-footer" data-test="cart-sticky-footer">
-      <button class="order-btn" data-test="order-button">35,300원 주문하기</button>
+    <button class="order-btn" data-test="order-button">
+      35,300원 주문하기
+    </button>
   </div>
 </template>
 
@@ -10,5 +12,24 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
+.sticky-footer {
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  height: 90px;
+  background-color: white;
+}
+
+.order-btn {
+  display: block;
+  width: calc(100% - 30px);
+  height: 50px;
+  margin: 15px 15px 30px 15px;
+  border-radius: 8px;
+  background-color: #ff5160;
+  color: white;
+  font-size: 18px;
+  font-weight: 700;
+}
 </style>
