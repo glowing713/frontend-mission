20e69d6276b9e1c9878c885030b7d589c16573a9
glowing713 <glowing713@naver.com>
2022-02-12 14:23:08 +0900

    test: Test sticky header in cartpage content container

--- mission/tests/unit/CartPage/CartPage.spec.js
+++ mission/tests/unit/CartPage/CartPage.spec.js
@@ -13,4 +13,30 @@ describe('CartPage', () => {
 
     expect(container.exists()).toBe(true);
   });
+
+  it('renders content container', () => {
+    const contents = wrapper.find('[data-test="cart-content-container"]');
+
+    expect(contents.exists()).toBe(true);
+  });
+
+  it('renders sticky header', () => {
+    const sticky = wrapper.find('[data-test="cart-sticky-header"]');
+
+    expect(sticky.exists()).toBe(true);
+  });
+
+  it('contains select all checkbox in sticky header', () => {
+    const sticky = wrapper.find('[data-test="cart-sticky-header"]');
+    const checkbox = sticky.find('[data-test="select-all"]');
+
+    expect(checkbox.exists()).toBe(true);
+  });
+
+  it('contains delete-from-cart button in sticky header', () => {
+    const sticky = wrapper.find('[data-test="cart-sticky-header"]');
+    const delButton = sticky.find('[data-test="delete-items"]');
+
+    expect(delButton.exists()).toBe(true);
+  });
 });
