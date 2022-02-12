2af9740c670a42ec774cbd2a56cb8398402b5e9e
glowing713 <glowing713@naver.com>
2022-02-12 14:23:33 +0900

    feat: Implement sticky header in cartpage

--- mission/src/views/CartPage.vue
+++ mission/src/views/CartPage.vue
@@ -1,6 +1,19 @@
 <template>
   <div class="cart-page" data-test="cart-page">
-      <cart-header />
+    <cart-header />
+    <div class="cart-contents" data-test="cart-content-container">
+      <div class="sticky-header" data-test="cart-sticky-header">
+        <p>
+          <input type="checkbox" class="select-all" data-test="select-all" />
+          <label for="select-all">전체선택 (2/2)</label>
+        </p>
+        <p>
+          <button class="delete-items" data-test="delete-items">
+            선택삭제
+          </button>
+        </p>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -15,7 +28,7 @@ export default {
 
 <style scoped>
 .cart-page {
-    width: 100%;
-    height: 100%;
+  width: 100%;
+  height: 100%;
 }
 </style>
