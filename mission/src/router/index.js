0c5bf88946d81e6ca78199634f8156e2beb76bbe
glowing713 <glowing713@naver.com>
2022-02-22 16:59:57 +0900

    refactor: Rename route object file

--- mission/src/router/index.js
+++ mission/src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import routes from './routes';
+import routes from './RoutesObj';
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
